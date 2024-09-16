b0d177bb14f8d561a77d4261ced3559b344b428c
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-09-16 19:48:05 -0300

    feat: add login view

--- src/ui/Login/Login.view.tsx
+++ src/ui/Login/Login.view.tsx
@@ -1,23 +1,35 @@
 'use client'
 
-import {
-  Flex,
-  Box,
-  Stack,
-  Heading,
-  useColorModeValue,
-} from '@chakra-ui/react'
+import { Flex, Box, Stack, Heading, useColorModeValue } from '@chakra-ui/react'
 import LoginForm from '@/ui/Login/LoginForm/login-form'
 import { Authentication } from '@/domain/usecases/authentication'
 
-export const LoginView = (authentication: Authentication) => {
+import {
+  SIGN_IN_TITLE_TEXT,
+} from '@/ui/Login/login-view.dictionary'
+
+interface LoginViewProps {
+  authentication: Authentication;
+}
+
+export const LoginView: React.FC<LoginViewProps> = ({ authentication }) => {
   return (
-    <Flex minH={'100vh'} align={'center'} justify={'center'} bg={useColorModeValue('gray.50', 'gray.800')}>
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}
+    >
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
         <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+          <Heading fontSize={'4xl'}>{SIGN_IN_TITLE_TEXT}</Heading>
         </Stack>
-        <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8}>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}
+        >
           <Stack spacing={4}>
             <LoginForm authentication={authentication} />
           </Stack>
