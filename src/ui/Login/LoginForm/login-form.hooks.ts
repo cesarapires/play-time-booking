260b927edd454ed40b19d898c41732ff31643ae7
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-11-15 13:11:36 -0300

    feat: add useFormValidations to validation form

--- src/ui/Login/LoginForm/login-form.hooks.ts
+++ src/ui/Login/LoginForm/login-form.hooks.ts
@@ -1,11 +1,15 @@
 import { Authentication } from '@/domain/usecases/authentication'
+import { useForm, UseFormReturn } from 'react-hook-form'
 import {
   LOGIN_FIELD,
   PASSWORD_FIELD,
 } from '@/ui/Login/LoginForm/login-form.consts'
+import { LoginFormValidation, LoginFormValidationType } from './login-form.validation'
+import { zodResolver } from '@hookform/resolvers/zod/src/zod.js'
 
 type UseLoginForm = {
   onSubmit: (formData: FormData) => void;
+  form: UseFormReturn<LoginFormValidationType>;
 };
 
 export const useLoginForm = (authentication: Authentication): UseLoginForm => {
@@ -21,7 +25,17 @@ export const useLoginForm = (authentication: Authentication): UseLoginForm => {
     }
   }
 
+  const form = useForm<LoginFormValidationType>({
+    resolver: zodResolver(LoginFormValidation),
+    defaultValues: {
+      login: '',
+      password: '',
+    },
+    mode: 'onBlur',
+  })
+
   return {
     onSubmit,
+    form
   }
 }
