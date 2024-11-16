d42f30155d8a581a0e32b4068a616f5d3bb96016
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-11-15 22:03:30 -0300

    feat: add component sidebar

---
+++ src/ui/common/components/sidebar/sidebar.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import {
+  Box,
+  useColorModeValue,
+  Drawer,
+  DrawerContent,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { MobileNav } from '@/ui/common/components/sidebar/mobile-nav'
+import { SidebarContent } from '@/ui/common/components/sidebar/sidebar-content'
+
+const SidebarWithHeader = ({ children }: { children: React.ReactNode }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: 'none', md: 'block' }}
+      />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full"
+      >
+        <DrawerContent>
+          <SidebarContent onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      <MobileNav onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        {children}
+      </Box>
+    </Box>
+  )
+}
+
+export default SidebarWithHeader
