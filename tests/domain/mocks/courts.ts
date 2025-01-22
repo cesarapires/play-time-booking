43c66c8ac0b9724a06d9f5b76175221d73182561
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-01-21 22:08:33 -0300

    chore: rename urlImage mock to return a real image

--- tests/domain/mocks/courts.ts
+++ tests/domain/mocks/courts.ts
@@ -5,19 +5,19 @@ export const mockCourtsList = (): Courts[] => {
     {
       publicId: '30c4204b-dd88-4814-becb-1ef3bef51f51',
       name: 'Court 1',
-      urlImage: 'http://image.com/image1.jpg',
+      urlImage: 'https://www.rioquente.com.br/images/attractions/0011/01.jpg',
       price: 'R$ 100,00',
     },
     {
       publicId: '2e9fdb75-e8e6-430d-a057-aa59c6a2b046',
       name: 'Court 2',
-      urlImage: 'http://image.com/image2.jpg',
+      urlImage: 'https://www.rioquente.com.br/images/attractions/0011/01.jpg',
       price: 'R$ 200,00',
     },
     {
       publicId: '29238bf1-947f-48ab-8fd2-c8017c44921a',
       name: 'Court 3',
-      urlImage: 'http://image.com/image3.jpg',
+      urlImage: 'https://www.rioquente.com.br/images/attractions/0011/01.jpg',
       price: 'R$ 300,00',
     },
   ]
